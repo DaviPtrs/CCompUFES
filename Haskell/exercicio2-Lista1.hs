a88fcf27629737496b259d1f7a68bac850680620
Davi Petris <37764531+DaviPtrs@users.noreply.github.com>
2018-03-29 23:24:17 -0300

    There's No Easter Eggs Here. Go Away!

---
+++ Haskell/exercicio2-Lista1.hs
@@ -0,0 +1,24 @@
+--b)Área do retângulo
+arrt x y = x*y 
+
+--c)Área do círculo
+arcir r = 3.14 * (r^2)
+
+--d)Distância euclidiana
+disteu xa ya xb yb = sqrt((xb - xa)^2 + (yb-ya)^2) 
+
+--e)Raizes de equação 2º grau
+delta a b c = (b^2) - 4 * a * c
+endx1 a b c = (-b + sqrt(delta a b c))/ (2*a)
+endx2 a b c = (-b - sqrt(delta a b c))/ (2*a)
+
+--f)Potência de expoente negativo
+exponeg a b =  1/(a^(abs (b)))
+
+--g)Conversão graus Celsius para Farenheit
+tempCF x = 1.8*x +32
+
+--h)Cálculo ganho anual de investimento com renda mensal
+gainyr v j = v*(1+(j/100))^12 - v
+
+--Davi de Souza Petris - CComp 2018--
