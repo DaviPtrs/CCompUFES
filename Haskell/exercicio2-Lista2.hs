e68d6a00bb1db2616792231425df5d362ca2578e
Davi Petris <37764531+DaviPtrs@users.noreply.github.com>
2018-05-03 15:02:09 -0300

    Update exercicio2-Lista2.hs

--- Haskell/exercicio2-Lista2.hs
+++ Haskell/exercicio2-Lista2.hs
@@ -5,15 +5,18 @@ inter35 x = (x>0) && (x<100) && ((mod x 3)==0) && ((mod x 5)==0)
 oux x y = (x||y) && not(x && y)
 
 --Número total de dias da execução de um projeto
-diasproj di mi ai df mf af = if (mi==mf) && (ai==af) 
-								then df-di 
+diasproj di mi ai df mf af = if (mi==mf) && (ai==af)
+								then df-di
 								else (df-di) + (msdias mf - msdias mi) + (ansdias af - ansdias ai)
 								where
 									msdias mes = mes*30
 									ansdias ano = ano*360
 
 --Analisa se 3 medidas dadas formam um triângulo ou não
-triang a b c = if (a<(b+c)) && (b<(a+c)) && (c<(a+b)) 
-					then putStrLn "As medidas PODEM formar um triângulo!"
-					else putStrLn "As medidas NÃO podem formar um triângulo!"
+triang a b c | (a<(b+c)) && (b<(a+c)) && (c<(a+b)) = putStrLn "As medidas PODEM formar um triângulo!"
+			 | otherwise = putStrLn "As medidas NÃO podem formar um triângulo!"
 
+--Pertinência triângul
+pertriang [x,y] [ex,ey] [dx,dy] = (chkP x dx ex]) && (chkP [y ey dy])
+									where
+										chkP p a b = (p<=a) && (p>=b)
