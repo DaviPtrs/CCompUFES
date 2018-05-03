8ea198272d8cb9f9926267caaeaeade4bcf3c30c
Davi Petris <37764531+DaviPtrs@users.noreply.github.com>
2018-05-03 11:04:36 -0300

    Add files via upload

---
+++ Haskell/exercicio2-Lista2.hs
@@ -0,0 +1,19 @@
+--Verificar se um número pertence ao intervalo (0,100) e é divisível por 3 e por 5
+inter35 x = (x>0) && (x<100) && ((mod x 3)==0) && ((mod x 5)==0)
+
+--Operador Ou (||) exclusivo
+oux x y = (x||y) && not(x && y)
+
+--Número total de dias da execução de um projeto
+diasproj di mi ai df mf af = if (mi==mf) && (ai==af) 
+								then df-di 
+								else (df-di) + (msdias mf - msdias mi) + (ansdias af - ansdias ai)
+								where
+									msdias mes = mes*30
+									ansdias ano = ano*360
+
+--Analisa se 3 medidas dadas formam um triângulo ou não
+triang a b c = if (a<(b+c)) && (b<(a+c)) && (c<(a+b)) 
+					then putStrLn "As medidas PODEM formar um triângulo!"
+					else putStrLn "As medidas NÃO podem formar um triângulo!"
+
