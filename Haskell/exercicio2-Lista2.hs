6aed97b451ed8a81baceb53f09e1e524e5c18b61
Davi Petris <37764531+DaviPtrs@users.noreply.github.com>
2018-05-04 20:31:09 -0300

    error ')' fixed

--- Haskell/exercicio2-Lista2.hs
+++ Haskell/exercicio2-Lista2.hs
@@ -3,9 +3,9 @@ div5 x = (mod x 5)==0 --Verifica se X é divisível por 5
 div3 x = (mod x 3)==0 --Verifica se X é divisível por 3
 chkaP x a b = (x>a) && (x<b) --Verifica se o valor P está no intervalo aberto de A e B
 chkP x a b = (x>=a) && (x<=b) --Verifica se o valor P está no intervalo fechado de A e B
-disteu [xa,ya] [xb,yb] = sqrt(xb - xa)^2 + (yb-ya)^2) --Distância entre 2 pontos no ponto
---mreta [x1,y1] [x2,y2] = (y2-y1)/(x2-x1) --Ângulo da reta
---ynox [x1,y1] [x2,y2] = (mreta [x1,y1] [x2,y2])*(x2-x1)+y1
+disteu [xa,ya] [xb,yb] = sqrt(xb - xa)^2 + (yb-ya)^2 --Distância entre 2 pontos no ponto
+mreta [x1,y1] [x2,y2] = (y2-y1)/(x2-x1) --Ângulo da reta
+ynox [x1,y1] [x2,y2] = (mreta [x1,y1] [x2,y2])*(x2-x1)+y1
 -------------------------------------------------------------------------------------------
 
 --Verificar se um número pertence ao intervalo (0,100) e é divisível por 3 e por 5
@@ -29,7 +29,7 @@ triang a b c    | (a<(b+c)) && (b<(a+c)) && (c<(a+b)) = putStrLn "As medidas POD
 pertriang [x,y] [ex,ey] [dx,dy] = (chkP x ex dx) && (chkP y dy ey)
 
 --Pertinência Losângulo 
---pertlosang [xe,ye] [xs,ys] [xp,yp] = yp <= (ynox [xe,ye] [xs,ys]) && (yp >= (ynox [xe,ye] [xs,(ys-2*ye)])) && (yp <= (ynox [(xe+2*xs),ye] [xs,ys])) && yp >= ynox [xe,ye] [xs,(ys-2*ye)]    
+pertlosang [xe,ye] [xs,ys] [xp,yp] = yp <= (ynox [xe,ye] [xs,ys]) && (yp >= (ynox [xe,ye] [xs,(ys-2*ye)])) && (yp <= (ynox [(xe+2*xs),ye] [xs,ys])) && yp >= ynox [xe,ye] [xs,(ys-2*ye)]    
 
 --Pertinência Círculo
 pertcirc r [xc,yc] [xp,yp] = chkP (disteu [xc,yc] [xp,yp]) 0 r
