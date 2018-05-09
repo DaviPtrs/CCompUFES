9a58ebc6767cbd883bb86a3300f8031ffc976c67
DaviPtrs <davispetris@gmail.com>
2018-05-09 18:42:16 -0300

    arrumando bagunça

--- Haskell/exercicio2-Lista2.hs
+++ Haskell/exercicio2-Lista2.hs
@@ -1,11 +1,11 @@
-------------------------------------------MÓDULOS------------------------------------------
+------------------------------------------------MÓDULOS--------------------------------------------------
 div5 x = (mod x 5)==0 --Verifica se X é divisível por 5
 div3 x = (mod x 3)==0 --Verifica se X é divisível por 3
 chkaP x a b = (x>a) && (x<b) --Verifica se o valor P está no intervalo aberto de A e B
 chkP x a b = (x>=a) && (x<=b) --Verifica se o valor P está no intervalo fechado de A e B
 disteu [xa,ya] [xb,yb] = sqrt(xb - xa)^2 + (yb-ya)^2 --Distância entre 2 pontos no ponto
 ynop [x,y] [xe,ye] [xs,ys] = ((ys-ye)/(xs-xe))*(x-xe)+ye --Tira o y de um ponto em relação a uma reta
--------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
 
 --A)Verificar se um número pertence ao intervalo (0,100) e é divisível por 3 e por 5
 inter35 x = (chkaP x 0 100) && (div3 x) && (div5 x)
@@ -40,16 +40,15 @@ pertlosang [x,y] [xe,ye] [xs,ys] = (y <= retaES) && (y >= retaESi) && (y <= reta
 pertcirc r [xc,yc] [xp,yp] = chkP (disteu [xc,yc] [xp,yp]) 0 r
 
 --F)Relação entre 3 números
-relaNum [a,b,c] | igualn3 [a,b,c] = putStrLn "Os três são iguais!"
-                | igual2n3 [a,b,c] = putStrLn "Apenas dois são iguais!"
+relaNum a b c | igualn3 a b c = putStrLn "Os três são iguais!"
+                | igual2n3 a b c = putStrLn "Apenas dois são iguais!"
                 | otherwise = putStrLn "Os três são diferentes!"
                 
                 where
-                    igualn3 [a,b,c] = (a==b) && (b==c)
-                    igual2n3 [a,b,c] = igualAB [a,b,c] || igualAC [a,b,c] || igualBC [a,b,c]
-                    igualAB [a,b,c] = (a==b) && (not(b==c))
-                    igualAC [a,b,c] = (a==c) && (not(b==c))
-                    igualBC [a,b,c] = (b==c) && (not(a==c))
+                    igualn3 a b c = (a==b) && (b==c)
+                    igual2n3 a b c = igual2a2 a b c || igual2a2 a c b || igual2a2 b c a
+                    igual2a2 a b c = (a==b) && (not(b==c))
+                    
 --G) Pertinência do retângulo no plano
 {--pertRetPlan [ex,ey] [dx,dy] | Chk1Q [ex,ey,dx,dy] || Chk2Q [ex,ey,dx,dy] || Chk3Q [ex,ey,dx,dy] || Chk4Q [ex,ey,dx,dy] = 1
                             | 
