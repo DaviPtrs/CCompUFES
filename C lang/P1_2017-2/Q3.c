52ad425e94d4e7b3740846c7689b0bee36eb8270
Davi-Manjaro <davispetris@gmail.com>
2018-08-25 23:58:23 -0300

    Questão 3 P1_2017-2

---
+++ C lang/P1_2017-2/Q3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int n1=1,n2=1,n3,soma=0,lim,i=0;
+    
+    printf("Limite da soma > ");
+    scanf("%d",&lim);
+
+    while (1){
+        n3 = n1+n2;
+        soma += n3;
+        if (soma > lim){
+            soma -= n2;
+            printf("%d ",n3);
+            break;
+        } else if (soma == lim){
+            printf("%d %d ",n2,n3);
+            break;
+        } else {
+            if (n1!=n2) printf("%d ",n2);
+        }
+        n1 = n2;
+        n2 = n3;
+        
+    }
+    printf("\nSoma dos termos: %d\n",soma);
+}
