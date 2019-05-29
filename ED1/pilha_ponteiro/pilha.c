644747733f0cc156d49ac67527810da9e52f666a
Davi Petris <37764531+DaviPtrs@users.noreply.github.com>
2019-05-28 21:11:47 -0300

    Update pilha.c

--- ED1/pilha_ponteiro/pilha.c
+++ ED1/pilha_ponteiro/pilha.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#define MAXTAM 50
 
 celulaPilha *criaCelula(int n){
     celulaPilha *tronco = malloc(sizeof(celulaPilha));
@@ -32,13 +33,6 @@ void liberaPilha(tPilha *litio){
     free(litio);
 }
 
-char *readSentence(){
-    char *sentence = malloc(50 *sizeof(char));
-    fgets(sentence, 50, stdin);
-
-    return sentence;
-}
-
 int convertCharToNum(char c){
     return(((int) c) - 48);
 }
@@ -47,121 +41,86 @@ int isOp(char c){
     return((c == '-') || (c == '+') || (c == '*') || (c == '/'));
 }
 
-void func(char *sentence, int tam){
-    int flag = 0;
-    int sinal = 0;
-    int ini = 0;
-    int num = 0;
+int calcIni(char *input, int tam){
+    //flag// 0 - numero; 1 - operação; 2 - syntax error
+    int flag, sinal, operou, ini, num, contNums, contOp;
+    flag = sinal = operou = ini = num = contNums = contOp = 0;
+
     for(int i = ini; i<tam; i++){
-        if(!isdigit(sentence[i]) && isOp(sentence[i])){
-            if((i == ini) && isdigit(sentence[i+1]) && (sentence[i] == '-'))
-                sinal = 1;
-            else
-                printf("Operação %c\n", sentence[i]);
-                num = 0;           
-        }else if(isdigit(sentence[i])){
+        if(!isdigit(input[i]) && isOp(input[i])){
+            if((i == ini) && isdigit(input[i+1])){
+                if(input[i] == '-')
+                    sinal = 1;
+                else if(input[i] == '+')
+                    sinal = 0;
+                else{
+                    flag = 2;
+                    break;
+                }
+            }
+            else{
+                flag = 1;
+                operou = 1;
+                contOp++;
+                printf("Operação %c\n", input[i]);
+            }
+        }else if(isdigit(input[i])){
             num *= 10;
-            num += convertCharToNum(sentence[i]);
-        }else if((sentence[i] == '\n') || (sentence[i] == '\0')){
+            num += convertCharToNum(input[i]);
+        }else if((input[i] == '\n') || (input[i] == '\0')){
             printf("Parou\n");
             break;
         }
         else{
-            if(sinal == 1) num = -num;
-            printf("Número %d\n", num);
+            if(flag == 0){
+                contNums++;
+                if(sinal == 1){
+                    sinal = 0;
+                    num = -num;
+                }
+                printf("Número %d\n", num);
+                num = 0;
+                flag = 0;
+            }
             ini = i + 1;
-            num = 0;
         }
+    }
+    free(input);
+
+    if(!operou || (contNums != (contOp + 1))) flag = 2; 
+    return flag;
+}
+
+void printPilha(tPilha *litio){
+    celulaPilha *aux = litio->Base;
+    for(int i = 0; aux!=NULL; i++){
+
     }
 }
 
-// int trataSentence(char *sentence, int tam, int *flag, int *ini){
-//     int num = 0;
-//     *flag = 0;
-//     for(int i = *ini; i<tam; i++){
-//         if(isdigit(sentence[i])){
-//             num *= 10;
-//             num += convertCharToNum(sentence[i]);
-//         }else{
-//             switch(sentence[i]){
-//                 case '-':
-//                     if((i == *ini) && isdigit(sentence[i+1])){
-//                         num = -num;
-//                     }else{
-//                         *flag = 1;
-//                         return (int)sentence[i];
-//                     }
-//                     break;
-//                 case '+':
-//                     if(!isdigit(sentence[i+1])){
-//                         *flag = 1;
-//                         return (int)sentence[i];
-//                     }                    
-//                     break;
-//                 case '*':
-//                     if(!isdigit(sentence[i+1])){
-//                         *flag = 1;
-//                         return (int)sentence[i];
-//                     }else{
-//                         *flag = 3;
-//                     }     
-//                     break;
-//                 case '/':
-//                     if(!isdigit(sentence[i+1])){
-//                         *flag = 1;
-//                         return (int)sentence[i];
-//                     }else{
-//                         *flag = 4;
-//                     }     
-//                     break;
-//                 case ' ':
-//                     *flag = 2;
-//                     *ini = i+1;
-//                     break;
-//                 case '\n':
-//                     *flag = 3;
-//                     break;
-//                 case '\0':
-//                     *flag = 3;
-//                     break;
-//                 default:
-//                     *flag = 4;
-//                     break;
-//             }        
-//         }
-//     }
+void printResultado(tPilha *calculos){
+    printf("=-=-=-=-=-=-=-=-=-=-=\n");
+    printf("Resultado = %d\n", calculos->Base->num);
+    printf("=-=-=-=-=-=-=-=-=-=-=\n");
 
-//     return num; // flag = 0 - num, flag = 1 - op, flag = 2 - espaço, flag = 3 - parada, flag = 4 - sinal inválido
-// }
+}
+
 
 int main(){
-    int flag, ini = 0;
 
-    char *frase = readSentence();
+    char *input = malloc(MAXTAM *sizeof(char));
+    fgets(input, MAXTAM, stdin);
 
-    func(frase, 50);
-
-    // while (1){
-    //     int num = trataSentence(frase, 50, &flag, &ini);
-    //     if(flag != 3){
-    //         switch(flag){
-    //             case 0:
-    //                 printf("Número %d\n", num);
-    //                 break;
-    //             case 1:
-    //                 printf("Operação %c\n", num);
-    //                 break;
-    //             case 4:
-    //                 printf("Sinal inválido! Finalizando...\n");
-    //                 exit(1);
-    //                 break;
-    //         }
-    //     }else{
-    //         printf("Parou\n");
-    //         break;
-    //     }
-    // }
+    tPilha *calculos = criaPilha();
     
+
+    if(calcIni(input, MAXTAM) == 2){
+        printf("=-=-SYNTAX ERROR-=-=\n");
+    }else{
+        // printResultado(calculos);
+    }
+
+    liberaPilha(calculos);
 
     return 0;
 }
