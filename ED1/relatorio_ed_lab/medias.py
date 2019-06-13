62f99f1fe938188c078c11c380a79605b392723f
Mint-VM <davispetris@gmail.com>
2019-06-13 00:45:41 -0300

    script pra calcular as médias de tempo

---
+++ ED1/relatorio_ed_lab/medias.py
@@ -0,0 +1,33 @@
+from os import system
+import csv
+
+testFiles = ["Arvore", "Lista", "AVL"]
+typeFiles = ["rand", "seq"]
+tamFiles = [100, 1000, 10000, 100000, 200000, 300000, 400000, 500000, 700000,
+            900000, 1000000, 2000000, 3000000]
+numBuscas = [10, 100, 1000]
+
+for struct in testFiles:
+    print("{}, médiaTempIns, qtdBusca, médiaTempBusca".format(struct))
+    for num in numBuscas:
+        for tipo in typeFiles:
+            for tam in tamFiles:
+                inputName = "input_{}_{}".format(tipo, tam)
+                fileName = "output/{}_{}_{}.csv".format(struct, inputName, num)
+                with open(fileName) as csvfile:
+                    file = csv.reader(csvfile, delimiter=',')
+                    line = 0
+                    avgTempIns = 0
+                    avgTempBusca = 0
+                    for row in file:
+                        if line < 2:
+                            line += 1
+                        else:
+                            avgTempIns += (float(row[0]) / 30)
+                            avgTempBusca += (float(row[2]) / 30)
+                rowName = "{} ({})".format(tam, tipo)
+                print("{},{:.6f},{:.6f},{:.6f}".format(rowName, avgTempIns, num, avgTempBusca))
+
+
+
+                        
--- ED1/relatorio_ed_lab/run.py
+++ ED1/relatorio_ed_lab/run.py
@@ -1,4 +1,5 @@
 from os import system
+import csv
 
 testFiles = ["Arvore", "Lista", "AVL"]
 typeFiles = ["rand", "seq"]
@@ -6,20 +7,26 @@ tamFiles = [100, 1000, 10000, 100000, 200000, 300000, 400000, 500000, 700000,
             900000, 1000000, 2000000, 3000000]
 numBuscas = [10, 100, 1000]
 
-
-
-
+system("mkdir output")
+system("rm output/*")
 for struct in testFiles:
     for num in numBuscas:
         for tipo in typeFiles:
             for tam in tamFiles:
-                inputName = "input_" + tipo + "_" + str(tam)
-                fileName = "output/" + inputName + "_" + str(num) + ".csv"
-                cmd = "./code/Desempenho" + struct + " " + str(num) + " < input/" + inputName + ".txt"
+                inputName = "input_{}_{}".format(tipo, tam)
+                fileName = "output/{}_{}_{}.csv".format(struct, inputName, num)
+                columns = "tempoInsert, qtdBuscas, tempoBusca"
+                cmd = "./code/Desempenho{} {} < input/{}.txt".format(struct, num, inputName)
                 print("Criando arquivo " + fileName)
                 system("touch "+ fileName)
-                system("echo '" + fileName + "' >> " + fileName)
+                system("echo '{}' >>  {}".format(fileName, fileName))
+                system("echo '{}' >>  {}".format(columns, fileName))
                 print(cmd)
                 for c in range(0,30):
                     system(cmd + " >> " + fileName)
+
+output = "output.csv"
+system("rm " + output)
+system("touch " + output)
+system("python3.6 medias.py > " + output)
                 
