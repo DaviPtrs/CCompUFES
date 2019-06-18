78fcc1853b3bc98bb50345625935dc39a1abc524
Davi Labgrad <davispetris@gmail.com>
2019-06-18 18:32:47 -0300

    Teste AVL e Lista feitos

--- ED1/relatorio_ed_lab/run.py
+++ ED1/relatorio_ed_lab/run.py
@@ -1,7 +1,7 @@
 from os import system
 import csv
 
-testFiles = ["Arvore", "Lista", "AVL"]
+testFiles = ["AVL", "Arvore"]
 typeFiles = ["rand", "seq"]
 tamFiles = [100, 1000, 10000, 100000, 200000, 300000, 400000, 500000, 700000,
             900000, 1000000, 2000000, 3000000]
