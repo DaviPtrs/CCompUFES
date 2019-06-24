5a3e4ded7baac5b70d1252a0d052eb6f836b792b
Davi - LAR <davispetris@gmail.com>
2019-06-24 14:18:33 -0300

    Fila com ponteiros aparentemente pronta

--- ED1/fila/fila_ponteiro/fila.h
+++ ED1/fila/fila_ponteiro/fila.h
@@ -1,12 +1,22 @@
 #ifndef FILA_H_
 #define FILA_H_
+#include "../fila_pilha/pilha.h"
 
-#include "pilha.h"
+typedef struct tfila tFila;
+struct tfila{
+	Pessoa *pessoa;
+	tFila *prox;
+};
 
 /*Tipo que define a fila (tipo opaco)
-  Estrutura interna do tipo deve ser definida na implementação do TAD. Usar como base o TAD Pilha (pilha.h) - não usar outra estrutura de dados
+  Estrutura interna do tipo deve ser definida na implementaï¿½ï¿½o do TAD. Usar como base o TAD Pilha (pilha.h) - nï¿½o usar outra estrutura de dados
   */
 typedef struct fila Fila;
+struct fila{
+	int topo;
+	tFila *index;
+  tFila *fim;
+};
 
 /*Cria uma fila vazia, usando pilha
 * inputs: nenhum
@@ -18,22 +28,22 @@ Fila* cria_fila(void);
 /*Insere uma pessoa na fila de Pessoas (lembre-se, fila e' FIFO). Imprime mensagem de erro caso a fila esteja cheia.
 * inputs: pessoa a ser inserida na fila (do tipo pessoa) e a fila
 * output: nenhum 
-* pre-condicao: pessoa e fila não são nulos
-* pos-condicao: fila contém a pessoa inserida na ultima posição*/
+* pre-condicao: pessoa e fila nï¿½o sï¿½o nulos
+* pos-condicao: fila contï¿½m a pessoa inserida na ultima posiï¿½ï¿½o*/
 void insere (Pessoa* pessoa, Fila* f);
 
 /*Retira uma pessoa da fila (usando FIFO). Imprime mensagem de erro caso a fila esteja vazia.
 * inputs: a fila
 * output: a Pessoa (do tipo Pessoa) retirada da Fila (ou NULL, caso a fila esteja vazia)
-* pre-condicao: fila não é nula
-* pos-condicao: fila não contém a pessoa que estava na primeira posicao */
+* pre-condicao: fila nï¿½o ï¿½ nula
+* pos-condicao: fila nï¿½o contï¿½m a pessoa que estava na primeira posicao */
 Pessoa* retira (Fila* f);
 
 
 /*Imprime os dados de todos as pessoas da fila (do inicio ao fim da fila!). Imprime mensagem de fila vazia, caso a fila esteja vazia.
 * inputs: a fila de pessoas
 * output: nenhum
-* pre-condicao: fila não é nula
+* pre-condicao: fila nï¿½o ï¿½ nula
 * pos-condicao: dados dos pessoas impressos na saida padrao
 */
 void imprime_fila (Fila* f);
@@ -42,7 +52,7 @@ void imprime_fila (Fila* f);
 /*Verifica se a fila esta ou nao vazia
 * inputs: a fila de pessoas
 * output: 1 se estiver vazia e zero caso contrario
-* pre-condicao: fila não é nula
+* pre-condicao: fila nï¿½o ï¿½ nula
 * pos-condicao: nenhuma
 */
 int Vazia_fila (Fila* f);
@@ -51,16 +61,16 @@ int Vazia_fila (Fila* f);
 /*Separa a fila f de tal forma que as pessoas com idade maior que 60 sejam movidas para a fila "f_maiores" e as pessoas com idade menor que 60 para a fila "f_menores".
 * inputs: a fila de pessoas, as filas f_maiores e f_menores (inicialmente vazias)
 * output: nao tem
-* pre-condicao: fila f não é nula
+* pre-condicao: fila f nï¿½o ï¿½ nula
 * pos-condicao: fila f vazia, pessoas com idade maior ou igual a 60 na fila "f_maiores" e as pessoas com idade menor que 60 para a fila "f_menores" */
 void separa_filas (Fila* f, Fila* f_maiores, Fila* f_menores);
 
 
-/*Libera toda a memória utilizada pelas estruturas de dados.
+/*Libera toda a memï¿½ria utilizada pelas estruturas de dados.
 * inputs: a fila de pessoas
 * output: NULL
-* pre-condicao: fila f não é nula
-* pos-condicao: Memória liberada */
+* pre-condicao: fila f nï¿½o ï¿½ nula
+* pos-condicao: Memï¿½ria liberada */
 Fila* destroi_fila (Fila* f);
 
 
